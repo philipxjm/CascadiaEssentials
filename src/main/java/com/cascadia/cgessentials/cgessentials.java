5ad05652b0f65365aaa4e55c2afb307ddd85bbae
XLuma <xenoversking@gmail.com>
2021-01-19 19:36:59 -0400

    Added item setup + first item

--- src/main/java/com/cascadia/cgessentials/cgessentials.java
+++ src/main/java/com/cascadia/cgessentials/cgessentials.java
@@ -1,9 +1,11 @@
 package com.cascadia.cgessentials;
 
+import com.cascadia.cgessentials.core.init.ItemInit;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
@@ -19,15 +21,20 @@ import org.apache.logging.log4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("cgessentials")
+@Mod(cgessentials.MOD_ID)
 public class cgessentials
 {
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
+    public static final String MOD_ID = "cgessentials";
 
     public cgessentials() {
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+        bus.addListener(this::setup);
+
+        ItemInit.ITEMS.register(bus);
         // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+       // FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         // Register the enqueueIMC method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
         // Register the processIMC method for modloading
@@ -37,6 +44,7 @@ public class cgessentials
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
+
     }
 
     private void setup(final FMLCommonSetupEvent event)
@@ -54,7 +62,7 @@ public class cgessentials
     private void enqueueIMC(final InterModEnqueueEvent event)
     {
         // some example code to dispatch IMC to another mod
-        InterModComms.sendTo("examplemod", "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
+        InterModComms.sendTo("ex    ", "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
     }
 
     private void processIMC(final InterModProcessEvent event)
--- src/main/java/com/cascadia/cgessentials/core/init/ItemInit.java
+++ src/main/java/com/cascadia/cgessentials/core/init/ItemInit.java
@@ -1,4 +1,16 @@
 package com.cascadia.cgessentials.core.init;
 
+import com.cascadia.cgessentials.cgessentials;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
 public class ItemInit {
+
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, cgessentials.MOD_ID);
+
+    public static final RegistryObject<Item> PARMESAN_BALL = ITEMS.register("parmesan_ball",
+            () -> new Item(new Item.Properties().group(ItemGroup.FOOD)));
 }
